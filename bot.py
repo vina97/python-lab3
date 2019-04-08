a1af624eb5e3f3bf5937a38a8bb1f1f4237ef040
vina97 <vinai@hotmail.it>
2019-04-08 16:01:03 +0200

    fix bug

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-from telegram.ext import messagehandler
+from telegram.ext import MessageHandler
 from telegram.ext import Filters
 
 file = open("task_list.txt")
@@ -19,19 +19,18 @@ def start(bot, update):
 
 def showTasks(bot, update):
     if tasks.__len__() == 0:
-        bot.message.reply_text("nothing to do here!\n")
+        bot.send_message(chat_id=update.message.chat_id, text="nothing to do here!")
     else:
+        message = ''
         for elem in sorted(tasks):
-            bot.message.reply_text(elem)
+            message = message + elem
+        bot.send_message(chat_id=update.message.chat_id, text=message)
 
 def newTask(bot, update, args):
     task = ' '.join(args)
     tasks.append(task)
-    file = open("task_list.txt", 'w')
-    for elem in tasks:
-        file.write(elem + "\n")
-    file.close()
     bot.send_message(chat_id=update.message.chat_id, text="new task added!")
+    savefile()
 
 
 
@@ -39,13 +38,18 @@ def removeTask(bot, update, args):
     task = ' '.join(args)
     if tasks.__contains__(task):
         tasks.remove(task)
-        bot.message.reply_text("task correctly deleted!\n")
-        file = open("task_list.txt", 'w')
-        for elem in tasks:
-            file.write(elem + "\n")
-        file.close()
+        bot.send_message(chat_id=update.message.chat_id, text="task correctly deleted!")
     else:
-        bot.message.reply_text("task not found!\n")
+        bot.send_message(chat_id=update.message.chat_id, text="task not found!")
+    savefile()
+
+
+
+def savefile():
+    file = open("task_list.txt", 'w')
+    for elem in tasks:
+        file.write(elem + "\n")
+    file.close()
 
 
 def removeAllTasks(bot, update, args):
@@ -55,17 +59,33 @@ def removeAllTasks(bot, update, args):
         if elem.__contains__(t):
             count = count + 1
             tasks.remove(elem)
-            bot.message.reply_text(elem + " correctly deleted!\n")
+            t = elem + " correctly deleted"
+            bot.send_message(chat_id=update.message.chat_id, text=t)
     if count == 0:
         bot.message.reply_text("I did not find any task to delete!\n")
+    savefile()
 
+def echo(bot, update):
+    receivedText = update.message.text
+    textToSend = "I'm sorry, I can't do that"
+    bot.sendMessage(chat_id=update.message.chat_id, text=textToSend)
 
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('showTasks', showTasks))
-dispatcher.add_handler(CommandHandler('newTask', newTask, pass_args=True))
-dispatcher.add_handler(CommandHandler('removeTask', removeTask, pass_args=True))
-dispatcher.add_handler(CommandHandler('removeAllTasks', removeAllTasks, pass_args=True))
 
+showTask_handler = dispatcher.add_handler(CommandHandler('showTasks', showTasks))
+dispatcher.add_handler(showTask_handler)
+
+newTask_handler = dispatcher.add_handler(CommandHandler('newTask', newTask, pass_args=True))
+dispatcher.add_handler(newTask_handler)
+
+removeTask_handler = dispatcher.add_handler(CommandHandler('removeTask', removeTask, pass_args=True))
+dispatcher.add_handler(removeTask_handler)
+
+removeAllTask_handler = dispatcher.add_handler(CommandHandler('removeAllTasks', removeAllTasks, pass_args=True))
+dispatcher.add_handler(removeAllTask_handler)
+
+dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
 updater.start_polling()
+updater.idle()
 
